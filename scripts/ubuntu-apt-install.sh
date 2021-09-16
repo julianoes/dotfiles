4f505326ad462f3d6c2d2d453c6ced46694d54b4
Julian Oes <julian@oes.ch>
2021-09-16 10:02:27 +0200

    More apps to install

--- scripts/ubuntu-apt-install.sh
+++ scripts/ubuntu-apt-install.sh
@@ -8,7 +8,9 @@ sudo apt dist-upgrade -y
 sudo add-apt-repository --yes ppa:neovim-ppa/unstable
 sudo apt install neovim
 
-sudo apt install git zsh fzf
+sudo apt install git zsh fzf htop
 
 sudo add-apt-repository ppa:regolith-linux/release
 sudo apt install i3-gaps i3blocks
+
+sudo apt install gimp
