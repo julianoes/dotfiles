9545b383f775ac24ab5f4f4ce30de6c000afd163
Julian Oes <julian@oes.ch>
2024-05-04 15:29:36 +1200

    scripts: cleanup
    Signed-off-by: Julian Oes <julian@oes.ch>

---
+++ scripts/apt-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt update
+sudo apt dist-upgrade -y
+
+sudo apt install -y
+    neovim \
+    xclip \
+    git \
+    zsh \
+    fzf \
+    htop \
+    ripgrep \
+    fd-find \
+    tree \
+    meld \
+    build-essential \
+    cmake \
+    clangd \
+    curl \
+    dtrx
